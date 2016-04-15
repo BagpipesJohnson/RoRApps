30fa14d088eddbb8aa4a39530915c4596958437c
BagpipesJohnson <christopherjohnsonpiper@gmail.com>
2016-04-15 11:21:39 -0600

    still working

--- first_ruby_app/blog/app/controllers/comments_controller.rb
+++ first_ruby_app/blog/app/controllers/comments_controller.rb
@@ -12,6 +12,10 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
  
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
